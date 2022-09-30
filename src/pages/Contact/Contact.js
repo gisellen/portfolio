1ca6039e6f0f7b76a6c76dc0af4311d80e58cc63
Gisellen <giselle.domingo.x@gmail.com>
2022-09-30 18:12:59 -0400

    Added contact visuals

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,9 +1,16 @@
 import React from 'react'
-
+import './Contact.css'
 export default function Contact() {
   return (
     <div className='contact'>
-        <h1 className="header-title">contact</h1>
+        <h1 className="header-title header-contact">contact</h1>
+        <form className='contact-form' onSubmit={() => {return false}}>
+            <input type='text' placeholder='Name' />
+            <input type='email' placeholder='Email' />
+            <input type='text' placeholder='Subject' />
+            <textarea name='text' placeholder='Description' ></textarea>
+            <input type='submit' />
+        </form>
     </div>
   )
 }
