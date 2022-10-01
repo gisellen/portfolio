5d1ec0171a190e8c4e27755a64e671a7473e0305
Gisellen <giselle.domingo.x@gmail.com>
2022-09-30 21:03:46 -0400

    Added function to send emails

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,15 +1,30 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import emailjs from '@emailjs/browser'
 import './Contact.css'
+
 export default function Contact() {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs.sendForm('service_id', 'template_id', form.current, 'public_key')
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+      };
+
   return (
     <div className='contact'>
         <h1 className="header-title header-contact">contact</h1>
-        <form className='contact-form' onSubmit={() => {return false}}>
-            <input type='text' placeholder='Name' />
-            <input type='email' placeholder='Email' />
-            <input type='text' placeholder='Subject' />
-            <textarea name='text' placeholder='Description' ></textarea>
-            <input type='submit' />
+        <form className='contact-form' ref={form} onSubmit={sendEmail}>
+            <input type='text' name='name' placeholder='Name' />
+            <input type='email' name='email' placeholder='Email' />
+            <input type='text' name='subject' placeholder='Subject' />
+            <textarea name='message' placeholder='Message' ></textarea>
+            <input type='submit' value="Send" />
         </form>
     </div>
   )
