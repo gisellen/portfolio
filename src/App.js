95be403a13a9ca3df851a39a7c81e41ca914838b
Gisellen <giselle.domingo.x@gmail.com>
2022-12-06 12:43:59 -0500

    Added basic loading page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import "./App.css";
 import Main from "./pages/Main";
+import React, { useState, useEffect} from 'react'
 
 //TODO: add back to top functions
 
 function App() {
+  const [loading, setLoading] = useState(false)
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
   return (
-    <div>
-      <Main />
+    <div className="container">
+      {loading ? (
+        <div className="loader-container">
+          <div className="circle"></div>
+          <div className="triangle"></div>
+          <div className="x"></div>
+        </div>
+      ) : (<Main />)}
+      
     </div>
   );
 }
